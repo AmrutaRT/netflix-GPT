46184ba3f51dce7f55e61d2e1227278992a6a50b
AmrutaRT <amru.rudrawar@gmail.com>
2025-09-05 13:43:22 -0700

    header

---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+    return (
+        <div>
+           <Header/>
+           <div>
+           <img src="https://assets.nflxext.com/ffe/siteui/vlv3/8d617e19-3c3c-4c28-8998-c9b14dbc7200/web/US-en-20250901-TRIFECTA-perspective_a7b980b6-8e93-4f18-9a62-56d5812c4956_large.jpg" srcset="https://assets.nflxext.com/ffe/siteui/vlv3/8d617e19-3c3c-4c28-8998-c9b14dbc7200/web/US-en-20250901-TRIFECTA-perspective_a7b980b6-8e93-4f18-9a62-56d5812c4956_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/8d617e19-3c3c-4c28-8998-c9b14dbc7200/web/US-en-20250901-TRIFECTA-perspective_a7b980b6-8e93-4f18-9a62-56d5812c4956_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/8d617e19-3c3c-4c28-8998-c9b14dbc7200/web/US-en-20250901-TRIFECTA-perspective_a7b980b6-8e93-4f18-9a62-56d5812c4956_small.jpg 959w" alt="" aria-hidden="true" class="default-ltr-iqcdef-cache-19j6xtr"></img>
+           </div>
+        </div>
+    );
+};
+
+export default Login;
